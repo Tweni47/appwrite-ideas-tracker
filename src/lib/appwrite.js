854bd049b8b793ff8299da60d53d0b64f329ca55
Tweni47 <derrickayaya@gmail.com>
2024-07-11 14:13:27 +0300

    Setting Up Project

---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("668fbc3600383ab8494b"); // Replace with your project ID
+
+export const account = new Account(client);
+export const databases = new Databases(client);
